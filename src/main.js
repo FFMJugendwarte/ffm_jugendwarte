3c8bca392f3c0758986f9f30a4be3f28e5ba69d4
Sebastian <sebastian.leitner@gmx.de>
2025-04-06 21:12:19 +0200

    -m

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-
 import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// 🔐 NEU: AWS Amplify
+import { Amplify } from 'aws-amplify'
+import awsExports from './aws-exports'
+
+// Konfiguriere Amplify mit deinen Cognito-Daten
+Amplify.configure(awsExports)
 
 createApp(App).use(router).mount('#app')
