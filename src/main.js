c47ff648c43e40cec0d45f9ba982971326a43cb2
Sebastian <sebastian.leitner@gmx.de>
2025-04-06 22:10:45 +0200

    Error correction!

--- src/main.js
+++ src/main.js
@@ -3,16 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// 🔐 NEU: AWS Amplify
+// 🔐 AWS Amplify (ohne Auth-Konfiguration nötig)
 import { Amplify } from 'aws-amplify'
 import awsExports from './aws-exports'
 
-Amplify.configure({
-    ...awsExports,
-    Auth,
-})
-
-// Konfiguriere Amplify mit deinen Cognito-Daten
 Amplify.configure(awsExports)
 
 createApp(App).use(router).mount('#app')
