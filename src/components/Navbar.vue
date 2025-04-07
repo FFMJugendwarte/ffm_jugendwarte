89900173b65b936727269c41c22f2414583d4b0d
SebLeitner <60791829+SebLeitner@users.noreply.github.com>
2025-04-07 16:58:01 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,14 @@
 <template>
   <nav class="flex items-center justify-between bg-blue-600 p-4 text-white">
-    <div class="text-lg font-bold">FFM Jugendwarte</div>
+    <router-link to="/" class="text-lg font-bold hover:underline">FFM Jugendwarte</router-link>
     <div class="space-x-4">
-      <router-link to="/" class="hover:underline">Start</router-link>
-      <router-link to="/mitglieder" class="hover:underline">Mitglieder</router-link>
+      <router-link
+        v-if="isAuthenticated"
+        to="/mitglieder"
+        class="hover:underline"
+      >
+        Mitglieder
+      </router-link>
       <router-link
         v-if="route.path === '/mitglieder'"
         to="/neu"
@@ -11,14 +16,28 @@
       >
         Neu
       </router-link>
-      <button @click="logout" class="hover:underline text-red-200">Logout</button>
+      <button
+        v-if="isAuthenticated"
+        @click="logout"
+        class="hover:underline text-red-200"
+      >
+        Logout
+      </button>
+      <router-link
+        v-else
+        to="/login"
+        class="hover:underline"
+      >
+        Login
+      </router-link>
     </div>
   </nav>
 </template>
 
 <script setup>
 import { useRouter, useRoute } from 'vue-router'
-import { signOut } from 'aws-amplify/auth'
+import { computed } from 'vue'
+import { signOut, fetchAuthSession } from 'aws-amplify/auth'
 
 const router = useRouter()
 const route = useRoute()
@@ -27,4 +46,14 @@ const logout = async () => {
   await signOut()
   router.push('/login')
 }
+
+// Zustand der Authentifizierung prüfen
+const isAuthenticated = computed(async () => {
+  try {
+    const session = await fetchAuthSession()
+    return session.tokens !== undefined
+  } catch {
+    return false
+  }
+})
 </script>
