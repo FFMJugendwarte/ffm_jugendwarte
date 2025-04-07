6601827fd83cba824a6a2116590e0679d6094299
SebLeitner <60791829+SebLeitner@users.noreply.github.com>
2025-04-07 16:40:51 +0200

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,10 +1,10 @@
 const awsmobile = {
   aws_project_region: 'eu-central-1',
   aws_cognito_region: 'eu-central-1',
-  aws_user_pools_id: 'eu-central-1_R8HECeYpb',
-  aws_user_pools_web_client_id: '615b0u7h0d788m791ru47jpgcd',
+  aws_user_pools_id: 'eu-central-1_4AY11Vz5D',
+  aws_user_pools_web_client_id: '7ldra5pg0gfi0g6d2d6o55gcs4',
   oauth: {
-    domain: 'eu-central-1r8heceypb.auth.eu-central-1.amazoncognito.com',
+    domain: 'eu-central-14ay11vz5d.auth.eu-central-1.amazoncognito.com',
     redirectSignIn: 'https://ffmjw.leitnersoft.com/',
     redirectSignOut: 'https://ffmjw.leitnersoft.com/',
     responseType: 'code',
